ad7180c209864d44625d1fe0bd2a9b6ef254dbc2
DESKTOP-JUSC5N8\Administrator <zkti12345@naver.com>
2023-10-04 16:53:17 +0900

    연산자

---
+++ second/js/text-color.js
@@ -0,0 +1,6 @@
+   // heading 변수를 선언하고 h1태그를 저장한 꼴이 되었다.
+   var heading = document.querySelector("#heading");
+   //h1태그를 클릭을 하면 글자 색이 빨간색으로 설정하시오.
+   heading.onclick = function(){
+       heading.style.color = "red";
+   }
